62df9eea44b297ee45af21b24591b646369c0b4d
sunwooz <yangsunwoo@gmail.com>
2016-04-30 23:30:19 -0400

    add pry

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'indeed-ruby'
 
 gem 'devise'
 gem 'haml-rails'
+gem 'pry'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  def search(term, location)
-    
+  def search
+    #make an api call to indeed with the search query
   end
 end
