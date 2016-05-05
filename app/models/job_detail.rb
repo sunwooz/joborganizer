c78c5a77d66fd744143f5503e770e902a79cdfbf
sunwooz <yangsunwoo@gmail.com>
2016-05-05 12:58:23 -0400

    validates uniqueness of jobkey if user already has job detail saved with the same jobkey

--- app/models/job_detail.rb
+++ app/models/job_detail.rb
@@ -1,3 +1,10 @@
 class JobDetail < ActiveRecord::Base
   belongs_to :user
+  validates_uniqueness_of :jobkey, if: :jobkey_is_unique_to_user
+
+  private
+
+  def jobkey_is_unique_to_user
+    self.user.job_details.pluck(:jobkey).include?(self.jobkey)
+  end
 end
