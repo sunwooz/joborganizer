4092cb32c08eba082a293d7156c3693de1b2644d
sunwooz <yangsunwoo@gmail.com>
2016-05-04 20:54:28 -0400

    dry out code

--- app/controllers/job_details_controller.rb
+++ app/controllers/job_details_controller.rb
@@ -2,12 +2,11 @@ class JobDetailsController < ApplicationController
 
   def create
     job_detail = current_user.job_details.new(jobkey: params[:jobkey], user_id: params[:user_id])
-    if job_detail.save
-      respond_to do |format|
+    
+    respond_to do |format|
+      if job_detail.save
         format.js { render layout: false, action: "create_success" }
-      end
-    else
-      respond_to do |format|
+      else
         format.js { render layout: false, action: "create_failure" }
       end
     end
@@ -15,12 +14,11 @@ class JobDetailsController < ApplicationController
 
   def update
     job_detail = current_user.job_details.find(params[:job_detail_id])
-    if job_detail.update(strong_params)
-      respond_to do |format|
+    
+    respond_to do |format|
+      if job_detail.update(strong_params)
         format.js { render layout: false, action: "update_success" }
-      end
-    else
-      respond_to do |format|
+      else
         format.js { render layout: false, action: "update_failure" }
       end
     end
